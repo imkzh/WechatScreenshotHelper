f9f895cab6314c13647e85c9204524c571149577
Zhenghao Ke <imkzh>
2019-04-03 21:56:32 +0800

    Refined file structure.

--- make_injection.py
+++ make_injection.py
@@ -3,13 +3,13 @@ import re
 
 def main():
     sub = re.compile(r"`.*`", re.S)
-    with io.open("./fantacy.js", "r") as f:
+    with io.open("./extension/fantacy.js", "r") as f:
         fantacy = f.read()
 
     with io.open("./injected.js", "r") as f:
         injection = f.read()
 
-    with io.open("./fantacy.js", "w") as f:
+    with io.open("./extension/fantacy.js", "w") as f:
         f.write(sub.sub("`\n" + injection + "\n`", fantacy))
 
 
